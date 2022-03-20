b79e559bdd257f7addb939097d06deed139e8315
mikheys <mikheys@gmail.com>
2022-03-20 19:03:02 +0300

    q

---
+++ bot.py
@@ -0,0 +1,84 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+
+import markups as nav
+import text
+import text as txt
+
+logging.basicConfig(level=logging.INFO)
+
+token = os.environ['TOKEN']
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+# --- Основное меню ---
+@dp.message_handler(commands=["start"], commands_prefix="/")
+async def start_menu(message: types.Message):
+    await message.answer(txt.WELCOME, reply_markup=nav.startMenu)
+    await message.answer("Выберите нужный раздел ", reply_markup=nav.mainMenu)
+
+@dp.message_handler()
+async def bot_message(message: types.Message):
+    if message.text == "Меню":
+        # await message.delete()
+        await message.answer("Выберите нужный раздел ", reply_markup=nav.mainMenu)
+ #   else:
+    if message.text.lower() in text.EDA:
+        # await bot.send_message(message.from_user.id, "https://telegra.ph/eda-03-17")
+        await message.answer("https://telegra.ph/eda-03-17")
+        # await message.answer()
+        # await message.reply('Неизвестная команда')
+#        await message.delete()
+
+
+
+# --- Обработка пунктов меню ---
+@dp.callback_query_handler(text="emergency")
+async def emergency_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/emergency-03-16")
+    await message.answer()
+
+@dp.callback_query_handler(text="medical")
+async def medical_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/medical-03-16-3")
+    await message.answer()
+
+@dp.callback_query_handler(text="veterinary")
+async def veterinary_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/veterinary-03-16")
+    await message.answer()
+
+@dp.callback_query_handler(text="infovu")
+async def infovu_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/infovu-03-16")
+    await message.answer()
+
+@dp.callback_query_handler(text="transport")
+async def transport_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/transport-03-16")
+    await message.answer()
+
+@dp.callback_query_handler(text="taxi")
+async def taxi_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/taxi-03-16-2")
+    await message.answer()
+
+@dp.callback_query_handler(text="eda")
+async def eda_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/eda-03-17")
+    await message.answer()
+
+@dp.callback_query_handler(text="publicservice")
+async def publicservice_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/publicservice-03-16")
+    await message.answer()
+
+@dp.callback_query_handler(text="others")
+async def others_msg(message: types.Message):
+    await bot.send_message(message.from_user.id, "https://telegra.ph/others-03-16")
+    await message.answer()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True)
---
+++ markups.py
@@ -0,0 +1,29 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import types
+
+# --- Start button ---
+btnStart = KeyboardButton("Меню")
+startMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnStart)
+
+# --- Main menu ---
+mainMenu = InlineKeyboardMarkup(row_width=1)
+btnEmergency = InlineKeyboardButton(text="🚨 Экстренное", callback_data="emergency")
+btnMedical = InlineKeyboardButton(text="🌡 Медицинская помощь", callback_data="medical")
+btnVeterinary = InlineKeyboardButton(text="🦮 Ветеринарная помощь", callback_data="veterinary")
+btnInfovu = InlineKeyboardButton(text="🗳 Контакты, инфо ВУ", callback_data="infovu")
+btnTransport = InlineKeyboardButton(text="🚌 Общественный транспорт", callback_data="transport")
+btnTaxi = InlineKeyboardButton(text="🚕 Такси", callback_data="taxi")
+btnEda = InlineKeyboardButton(text="🍝 Еда", callback_data="eda")
+btnPublicservice = InlineKeyboardButton(text="🪠 Коммунальные услуги", callback_data="publicservice")
+btnOthers = InlineKeyboardButton(text="📷 Прочие услуги", callback_data="others")
+
+mainMenu.insert(btnEmergency)
+mainMenu.insert(btnMedical)
+mainMenu.insert(btnVeterinary)
+mainMenu.insert(btnInfovu)
+mainMenu.insert(btnTransport)
+mainMenu.insert(btnTaxi)
+mainMenu.insert(btnEda)
+mainMenu.insert(btnPublicservice)
+mainMenu.insert(btnOthers)
